5a49d4914c4b533bbf4d3e25c8bb60f2f9b93223
anasqadri-dev <anasqadri.dev@outlook.com>
2026-08-18 15:39:04 +0500

    feat: improve student class by adding constructor

--- LABs/LAB#02/sqe-library-management/src/Student.java
+++ LABs/LAB#02/sqe-library-management/src/Student.java
@@ -1,7 +1,14 @@
 public class Student {
+    private String name;
+    private int age;
+    public Student() {
+        this.name = "Anas";
+        this.age = 21;
+    }
+    
     /**
      * Adds a score to the student.
-     **/
+    **/
     public void addScore(int score){
         if (score < 0) {
             throw new IllegalArgumentException("Score cannot be negative");
